a74c51f205e1b46d403fa4ec5a4cb32989e47afc
Rupalshah17 <shah17rupal@gmail.com>
2023-08-11 16:46:21 +0530

    bg added

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Header from "../components/Header";
 import { about } from "../HeaderData";
-
+import Program from "../components/AboutUsPage/Program";
+import Navbar from '../components/BodyNavbar/BodyNavbar';
 export default function AboutPage() {
   return (
-      <div className="about">
+    <div className="about">
+      <Navbar/>
       <div className="header">
         <Header title={about.title} description={about.description} image={about.image} />
       </div>
       
-      div.
+      <section className="programs">
+        <Program/>
+      </section>
       </div>
   )
 }
