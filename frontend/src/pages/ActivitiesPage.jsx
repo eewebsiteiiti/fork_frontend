e60d7674234ef5287764b746ac0465b556e1a3cb
muskanadwani2000@gmail.com <shuffled720@gmail.com>
2024-06-07 12:35:35 +0530

    event import added

--- frontend/src/pages/ActivitiesPage.jsx
+++ frontend/src/pages/ActivitiesPage.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import Header from "../components/Header";
-import Navbar from "../components/BodyNavbar/BodyNavbar";
-import { activity } from "../HeaderData";
-import { Container, Grid } from "@mui/material";
-
+import React from "react"
+import Header from "../components/Header"
+import Navbar from "../components/BodyNavbar/BodyNavbar"
+import { activity } from "../HeaderData"
+import { Container, Grid } from "@mui/material"
+import Event from "../components/HomePage/Event"
 
 export default function ActivitiesPage() {
   return (
@@ -12,29 +12,15 @@ export default function ActivitiesPage() {
       <Header title={activity.title} />
       <Container sx={{ my: 5 }}>
         <Grid item>
-         
-              <>
-                <Grid item sm={6} md={4} my={3}>
-                  <a
-                    rel="noreferrer"
-                    href
-                    target="_blank"
-                  >
-                    <Event
-                      description
-                      title
-                      image
-                      day
-                      year
-                      date
-                      month
-                      time
-                    />
-                  </a>
-                </Grid>
-              </>
+          <>
+            <Grid item sm={6} md={4} my={3}>
+              <a rel="noreferrer" href target="_blank">
+                <Event description title image day year date month time />
+              </a>
+            </Grid>
+          </>
         </Grid>
       </Container>
     </>
-  );
+  )
 }
