c0e803e4d7cac99d6b784f701daee3acc351a9f5
protista <135408454+protista009@users.noreply.github.com>
2024-07-28 14:26:21 +0530

    correction

--- frontend/src/NavData.js
+++ frontend/src/NavData.js
@@ -85,8 +85,8 @@ const navs = {
       Research: "/labs/pg",
     },
     Timetable: {
-      "Btech (4th yr) & PG-PHD": "https://drive.google.com/file/d/1hdJBYR3bS31bJzRlZ9dNlcuKXnuzAXif/view?usp=drivesdk",
-      "Btech (2nd & 3rd yr)" : "https://drive.google.com/file/d/1hbacL_gul_YK2FSiU1ecnPDATSaTUJqX/view?usp=drivesdk",
+      "B. Tech. (4th yr) & PG-PHD": "https://drive.google.com/file/d/1hdJBYR3bS31bJzRlZ9dNlcuKXnuzAXif/view?usp=drivesdk",
+      "B. Tech. (2nd & 3rd yr)" : "https://drive.google.com/file/d/1hbacL_gul_YK2FSiU1ecnPDATSaTUJqX/view?usp=drivesdk",
       "Time slots" : "https://drive.google.com/file/d/1hiyQKYPU5iQFeiYhX5P1S__JQomjB9aU/view?usp=drivesdk"
     },
   },
