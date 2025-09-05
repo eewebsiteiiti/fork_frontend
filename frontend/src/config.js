6a4627d78a6462b284e6c5824d3ee1018a0f7563
dibbend8 <dibbaroy@gmail.com>
2025-09-05 12:57:07 +0530

    Add IMAGE_BASE and READS_BASE constants

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,7 @@
 // src/config.js
-// frontend/src/config.js
 export const API_BASE =
   process.env.REACT_APP_API_BASE || "https://ee-iiti.onrender.com";
-
+export const IMAGE_BASE =
+  process.env.REACT_APP_IMAGE_BASE || "https://ee-iiti.onrender.com";
+export const READS_BASE =
+  process.env.REACT_APP_READS_BASE || "https://ee-iiti.onrender.com";
