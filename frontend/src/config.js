d75cad307d1e94df09c7039315bd49826729ff44
dibbend8 <dibbaroy@gmail.com>
2025-09-05 12:49:06 +0530

    Add default API_BASE URL in config.js
    Set default API_BASE URL if environment variable is not defined.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,5 @@
 // src/config.js
-export const API_BASE = process.env.REACT_APP_API_BASE;
+// frontend/src/config.js
+export const API_BASE =
+  process.env.REACT_APP_API_BASE || "https://ee-iiti.onrender.com";
+
