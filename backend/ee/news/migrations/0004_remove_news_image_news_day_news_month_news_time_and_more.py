3d41e4526af07f2e43846a9be7886d5bab160384
dibbend8 <dibbaroy@gmail.com>
2025-09-09 20:19:43 +0530

    Update max_length of 'day' field in news model
    Increased max_length of 'day' field in news model from 15 to 100.

--- backend/ee/news/migrations/0004_remove_news_image_news_day_news_month_news_time_and_more.py
+++ backend/ee/news/migrations/0004_remove_news_image_news_day_news_month_news_time_and_more.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="news",
             name="day",
-            field=models.CharField(default=django.utils.timezone.now, max_length=15),
+            field=models.CharField(default=django.utils.timezone.now, max_length=100),
             preserve_default=False,
         ),
         migrations.AddField(
