e4ea74417dcd915024c94a9ab9781d96b0edb4e5
eewebsiteiiti <website.ee@iiti.ac.in>
2025-10-29 13:41:03 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 export const API_BASE =
-  process.env.REACT_APP_API_BASE || "https://ee-iiti.onrender.com";
+  process.env.REACT_APP_API_BASE || "https://eebackend.onrender.com";
 export const IMAGE_BASE =
-  process.env.REACT_APP_IMAGE_BASE || "https://ee-iiti.onrender.com";
+  process.env.REACT_APP_IMAGE_BASE || "https://eebackend.onrender.com";
 export const READS_BASE =
-  process.env.REACT_APP_READS_BASE || "https://ee-iiti.onrender.com";
+  process.env.REACT_APP_READS_BASE || "https://eebackend.onrender.com";
