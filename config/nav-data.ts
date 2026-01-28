089014909192b565dc1cf75c605a35f368172083
Ayush <ayush@socratic.co>
2026-01-28 19:48:49 +0530

    fix: timetable links

--- config/nav-data.ts
+++ config/nav-data.ts
@@ -123,8 +123,9 @@ export const navData: NavItem[] = [
       {
         label: 'Timetable',
         children: [
-          { label: 'B.Tech. (4th yr) & PG-Ph.D.', href: 'https://drive.google.com/file/d/1hdJBYR3bS31bJzRlZ9dNlcuKXnuzAXif/view', external: true },
-          { label: 'B.Tech. (2nd & 3rd yr)', href: 'https://drive.google.com/file/d/1hbacL_gul_YK2FSiU1ecnPDATSaTUJqX/view', external: true },
+          { label: 'B.Tech. (4th yr) & PG-Ph.D.', href: 'https://docs.google.com/spreadsheets/d/1llvIJUTPa_kp7bJURQb8tA6fwXvamqr-ZKDJPjC6ZV4/edit?gid=415267304#gid=415267304', external: true },
+          { label: 'B.Tech. (3rd yr)', href: 'https://docs.google.com/spreadsheets/d/1llvIJUTPa_kp7bJURQb8tA6fwXvamqr-ZKDJPjC6ZV4/edit?gid=2052617232#gid=2052617232', external: true },
+          { label: 'B.Tech. (2nd yr)', href: 'https://docs.google.com/spreadsheets/d/1llvIJUTPa_kp7bJURQb8tA6fwXvamqr-ZKDJPjC6ZV4/edit?gid=0#gid=0', external: true },
           { label: 'Course Slots', href: 'https://drive.google.com/file/d/1hiyQKYPU5iQFeiYhX5P1S__JQomjB9aU/view', external: true },
         ],
       },
