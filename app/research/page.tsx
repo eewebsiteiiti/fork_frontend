b0e04b5e67af77216b16ad78bc74dfa300181b18
Ayush <ayush@socratic.co>
2026-03-27 01:14:31 +0530

    update: research page redesign

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -1,5 +1,50 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Box } from '@mui/material';
 import PageLayout from '@/components/layout/PageLayout';
+
+const domains = [
+  {
+    title: 'CSP & RFM',
+    subtitle: 'Communications, Signal Processing & Radio Frequency Microwave',
+    faculty: [
+      'Prof. Ram Bilas Pachori',
+      'Prof. Prabhat Kumar Upadhyay',
+      'Prof. Vimal Bhatia',
+      'Prof. Sumit Gautam',
+      'Prof. Swaminathan R.',
+      'Prof. Appina Balasubramanyam',
+      'Prof. Dibbendu Roy',
+      'Prof. Vivek Kanhangad',
+      'Prof. Ayush Tripathi',
+      'Prof. Saptrishi Ghosh',
+      'Prof. Rinkee Chopra',
+      'Prof. Sharad Kumar Singh',
+    ],
+  },
+  {
+    title: 'PSPE',
+    subtitle: 'Power Electronics, Machines and Power Systems',
+    faculty: [
+      'Prof. Amod C. Umarikar',
+      'Prof. Vijay A. S.',
+      'Prof. Lokesh Kumar Dewangan',
+      'Prof. B Prathap Reddy',
+      'Prof. Trapti Jain',
+      'Prof. Shubhadeep Paladhi',
+    ],
+  },
+  {
+    title: 'VDN',
+    subtitle: 'VLSI Design & Nanoelectronics',
+    faculty: [
+      'Prof. Santosh Kumar Vishvakarma',
+      'Prof. Shaibal Mukherjee',
+      'Prof. Mukesh Kumar',
+      'Prof. Vipul Singh',
+      'Prof. Srivathsan Vasudevan',
+      'Prof. Abhinav Kranti',
+    ],
+  },
+];
 
 export default function ResearchPage() {
   return (
@@ -9,183 +54,53 @@ export default function ResearchPage() {
       backgroundImage="/images/banners/research.jpg"
     >
       <div className="bg_border">
-        <br />
-        <br />
-        <br />
-        <Grid container>
-          <Grid size={{ xs: 12, sm: 6 }}>
-            <Typography variant="h5" paddingLeft="40%" color="secondary.main">
-              Communications
-            </Typography>
-            <div id="rectangle-l-outside">
-              <div id="rectangle-l-inside">
-                <Typography component="div" fontSize="0.8rem">
-                  <ul>
-                    <li>Prof. Ram Bilas Pachori</li>
-                    <li>Prof. Prabhat Kumar Upadhyay</li>
-                    <li>Prof. Vimal Bhatia</li>
-                    <li>Prof. Sumit Gautam</li>
-                    <li>Prof. Swaminathan R.</li>
-                    <li>Prof. Appina Balasubramanyam</li>
-                    <li>Prof. Dibbendu Roy</li>
-                  </ul>
-                </Typography>
-              </div>
-            </div>
+        <Box sx={{ py: 6, px: { xs: 2, md: 4 } }}>
+          <Grid container spacing={4} justifyContent="center">
+            {domains.map((domain) => (
+              <Grid key={domain.title} size={{ xs: 12, sm: 6, md: 4 }}>
+                <Box
+                  sx={{
+                    border: '2px solid',
+                    borderColor: 'secondary.main',
+                    borderRadius: 2,
+                    overflow: 'hidden',
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <Box
+                    sx={{
+                      bgcolor: 'secondary.main',
+                      color: 'white',
+                      py: 2,
+                      px: 2,
+                      textAlign: 'center',
+                    }}
+                  >
+                    <Typography variant="h5" fontWeight="bold">
+                      {domain.title}
+                    </Typography>
+                    <Typography variant="body2" sx={{ opacity: 0.9, mt: 0.5 }}>
+                      {domain.subtitle}
+                    </Typography>
+                  </Box>
+                  <Box sx={{ p: 2, flexGrow: 1 }}>
+                    <Typography component="div" fontSize="0.85rem">
+                      <ul style={{ margin: 0, paddingLeft: '1.2rem' }}>
+                        {domain.faculty.map((name) => (
+                          <li key={name} style={{ marginBottom: '4px' }}>
+                            {name}
+                          </li>
+                        ))}
+                      </ul>
+                    </Typography>
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
           </Grid>
-          <Grid size={{ xs: 12, sm: 6 }}>
-            <Typography variant="h5" align="right" paddingRight="40%" color="secondary.main">
-              Signal and Image Processing
-            </Typography>
-            <div id="rectangle-r-outside">
-              <div id="rectangle-r-inside">
-                <Typography component="div" fontSize="0.8rem">
-                  <ul>
-                    <li>Prof. Ram Bilas Pachori</li>
-                    <li>Prof. Prabhat Kumar Upadhyay</li>
-                    <li>Prof. Vimal Bhatia</li>
-                    <li>Prof. Vivek Kanhangad</li>
-                    <li>Prof. Appina Balasubramanyam</li>
-                    <li>Prof. Ayush Tripathi</li>
-                  </ul>
-                </Typography>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-        <br />
-
-        <Grid container>
-          <Grid size={{ xs: 12, sm: 6 }}>
-            <Typography variant="h5" paddingLeft="40%" color="secondary.main">
-              Power Electronics and Electric Machines
-            </Typography>
-            <div id="rectangle-l-outside">
-              <div id="rectangle-l-inside">
-                <Typography component="div" fontSize="0.8rem">
-                  <ul>
-                    <li>Prof. Amod C. Umarikar</li>
-                    <li>Prof. Vijay A. S.</li>
-                    <li>Prof. Lokesh Kumar Dewangan</li>
-                    <li>Prof. B Prathap Reddy</li>
-                  </ul>
-                </Typography>
-              </div>
-            </div>
-          </Grid>
-
-          <Grid size={{ xs: 12, sm: 6 }}>
-            <Typography variant="h5" align="right" paddingRight="40%" color="secondary.main">
-              Power Systems
-            </Typography>
-            <div id="rectangle-r-outside">
-              <div id="rectangle-r-inside">
-                <Typography component="div" fontSize="0.8rem">
-                  <ul>
-                    <li>Prof. Trapti Jain</li>
-                    <li>Prof. Shubhadeep Paladhi</li>
-                    <li>Prof. Lokesh Kumar Dewangan</li>
-                  </ul>
-                </Typography>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-        <br />
-
-        <Grid container>
-          <Grid size={{ xs: 12, sm: 6 }}>
-            <Typography variant="h5" paddingLeft="40%" color="secondary.main">
-              VLSI Design
-            </Typography>
-            <div id="rectangle-l-outside">
-              <div id="rectangle-l-inside">
-                <Typography component="div" fontSize="0.8rem">
-                  <ul>
-                    <li>Prof. Santosh Kumar Vishvakarma</li>
-                    <li>Prof. Shaibal Mukherjee</li>
-                    <li>Prof. Mukesh Kumar</li>
-                    <li>Prof. Vipul Singh</li>
-                  </ul>
-                </Typography>
-              </div>
-            </div>
-          </Grid>
-          <Grid size={{ xs: 12, sm: 6 }}>
-            <Typography variant="h5" align="right" paddingRight="40%" color="secondary.main">
-              Radio Frequency and Microwave
-            </Typography>
-            <div id="rectangle-r-outside">
-              <div id="rectangle-r-inside">
-                <Typography component="div" fontSize="0.8rem">
-                  <ul>
-                    <li>Prof. Saptrishi Ghosh</li>
-                    <li>Prof. Rinkee Chopra</li>
-                  </ul>
-                </Typography>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-        <br />
-
-        <Grid container>
-          <Grid size={{ xs: 12, sm: 6 }}>
-            <Typography variant="h5" paddingLeft="40%" color="secondary.main">
-              Nanoelectronic Devices
-            </Typography>
-            <div id="rectangle-l-outside">
-              <div id="rectangle-l-inside">
-                <Typography component="div" fontSize="0.8rem">
-                  <ul>
-                    <li>Prof. Srivathsan Vasudevan</li>
-                    <li>Prof. Mukesh Kumar</li>
-                    <li>Prof. Shaibal Mukherjee</li>
-                    <li>Prof. Vipul Singh</li>
-                    <li>Prof. Abhinav Kranti</li>
-                  </ul>
-                </Typography>
-              </div>
-            </div>
-          </Grid>
-          <Grid size={{ xs: 12, sm: 6 }}>
-            <Typography variant="h5" align='right' paddingRight="40%" color="secondary.main">
-              Control Instrumentation And Optimization
-            </Typography>
-            <div id="rectangle-r-outside">
-              <div id="rectangle-r-inside">
-                <Typography component="div" fontSize="0.8rem">
-                  <ul>
-                    <li>Prof. Sharad Kumar Singh</li>
-                    <li>Prof. Srivathsan Vasudevan</li>
-                  </ul>
-                </Typography>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-        <br />
-
-        <Grid container>
-          <Grid size={{ xs: 12, sm: 6 }}>
-            <Typography variant="h5" align="left" paddingLeft="40%" color="secondary.main">
-              Machine Learning and Deep Learning
-            </Typography>
-            <div id="rectangle-l-outside">
-              <div id="rectangle-l-inside">
-                <Typography component="div" fontSize="0.8rem">
-                  <ul>
-                    <li>Prof. Ram Bilas Pachori</li>
-                    <li>Prof. Vivek Kanhangad</li>
-                    <li>Prof. Vimal Bhatia</li>
-                    <li>Prof. Dibbendu Roy</li>
-                  </ul>
-                </Typography>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-        <br />
+        </Box>
       </div>
     </PageLayout>
   );
