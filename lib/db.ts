903cd382f6abdc8eaeafdecc01c44401e2a3cd3b
Ayush <ayush@socratic.co>
2026-04-02 18:05:14 +0530

    update: disable wal mode

--- lib/db.ts
+++ lib/db.ts
@@ -4,8 +4,6 @@ import path from 'path';
 const dbPath = path.join(process.cwd(), 'database.sqlite');
 const db = new Database(dbPath);
 
-// Enable foreign keys and WAL mode for better performance
-db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
 
 export default db;
